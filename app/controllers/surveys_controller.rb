d26dc9f9ed4d9af22ea703f5cea96b1bec71ed46
antonpot <markoapotocnik@hotmail.com>
2015-08-12 17:59:09 +0200

    #update and #edit methods added to controller

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -16,14 +16,23 @@ class SurveysController < ApplicationController
   end
 
   def create
-    p "#"*99
-    p params
     survey = Survey.new(title: params[:survey][:title], user_id: current_user.id)
     if survey.save
-      redirect_to root_path
+      redirect_to edit_survey(survey)
     else
       flash[:notice] = 'Something went wrong'
       redirect_to root_path
     end
+  end
+
+  def edit
+    survey = Survey.find(params[:id])
+    p "#"*99
+    p params
+
+  end
+
+  def update
+
   end
 end
