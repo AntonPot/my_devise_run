ebd59435e3cd6250e99cc5447851f31d853a0c85
antonpot <markoapotocnik@hotmail.com>
2015-08-12 17:51:17 +0200

    User can create a survey with a title

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -11,7 +11,19 @@ class SurveysController < ApplicationController
   end
 
   def new
-    survey = Survey.new
+    @survey = Survey.new
 
+  end
+
+  def create
+    p "#"*99
+    p params
+    survey = Survey.new(title: params[:survey][:title], user_id: current_user.id)
+    if survey.save
+      redirect_to root_path
+    else
+      flash[:notice] = 'Something went wrong'
+      redirect_to root_path
+    end
   end
 end
