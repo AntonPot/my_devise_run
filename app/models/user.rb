fbd97b2e136e56953699566e861c1e4e517a9d13
antonpot <markoapotocnik@hotmail.com>
2015-08-18 18:33:26 +0200

    Minor fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
-         :confirmable,
-         :omniauthable, omniauth_providers: [:linkedIn]
+         :confirmable
+         # :omniauthable, omniauth_providers: [:facebook]
          #:lockable, :timeoutable
 
   has_many :surveys
